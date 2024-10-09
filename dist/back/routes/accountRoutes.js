0e0a0e36042d10749b7cf9d6c45be79da8992cb2
Kirill <jopa5797@gmail.com>
2024-10-09 16:02:20 +0300

    pres

--- dist/back/routes/accountRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const accountController_1 = require("../controllers/accountController");
-const router = (0, express_1.Router)();
-router.post('/accounts', accountController_1.registerAccount);
-router.post('/accounts/login', accountController_1.loginAccount);
-router.get('/getUsername', accountController_1.fetchUsername);
-exports.default = router;
--- dist/back/routes/currencyRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const currencyController_1 = require("../controllers/currencyController");
-const router = (0, express_1.Router)();
-router.get('/currencies', currencyController_1.getCurrencies);
-exports.default = router;
